3a0ebd0e3e1e0e16e7459ec5964fe65f6e65fbe6
Jackson Gray <nohaxjustdoge@gmail.com>
2024-04-04 20:26:21 +0000

    putting thi on my resume

---
+++ target/debug/deps/rust_helloworld-ff1f9b874e5af20f.d
@@ -0,0 +1,5 @@
+/home/astro/Development/rust/rust-helloworld/target/debug/deps/rust_helloworld-ff1f9b874e5af20f: src/main.rs
+
+/home/astro/Development/rust/rust-helloworld/target/debug/deps/rust_helloworld-ff1f9b874e5af20f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust-helloworld.d
@@ -0,0 +1,1 @@
+/home/astro/Development/rust/rust-helloworld/target/debug/rust-helloworld: /home/astro/Development/rust/rust-helloworld/src/main.rs
